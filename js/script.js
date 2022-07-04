0af4dea9e7dfeade2e2a158e6af940b7803a2e92
Bogdan <bodya.kalashnyk09@yahoo.com>
2022-07-04 11:56:31 +0300

    practise 1

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ console.log(number);
 	let result = 50;
 	console.log(result);
 }
+
+// Практика 1
+const numberOffFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOffFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+	b = prompt("На сколько оцените его?", ""),
+	c = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+	d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
